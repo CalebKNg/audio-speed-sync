a0cca8792eaee0f60bb2af926220cebb6c500e31
CalebKNg <caleb17ng@gmail.com>
2026-01-15 13:50:03 -0800

    Centralize some styles

---
+++ app/styles/common.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const common = StyleSheet.create({
+    pageView: {
+        flex: 1,
+        backgroundColor: "#25292e",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+})
+
