ed8e594d1b8a31c4afc00d1a00428532092c2fce
CalebKNg <caleb17ng@gmail.com>
2026-01-20 11:52:53 -0800

    add metadata reading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import { StyleSheet, View } from 'react-native';
+// import 'react-native-polyfill-globals/auto';
+import { Buffer } from 'buffer';
 import { Provider } from "react-redux";
 import { AudioProvider } from "../components/audioProvider";
 import MiniPlayer from "../components/miniPlayer";
 import Player from "../components/player";
 import { store } from "./store";
 
+(global as any).Buffer = Buffer; // make Buffer available globally
 export default function RootLayout() {
   return (
     <AudioProvider>
--- src/app/features/scanner/scanLibrary.ts
+++ src/app/features/scanner/scanLibrary.ts
@@ -1,13 +1,16 @@
 import * as MediaLibrary from 'expo-media-library';
 
+import * as mm from 'music-metadata';
+import RNFS from 'react-native-fs';
+
+
 export type AudioAssetWithMetadata = {
     id: string;
     uri: string;
     artist: string;
     album: string;
     title: string;
-    genre: string;
-    year: string;
+    picture?: mm.IPicture;
 };
 
 async function requestPermission() {
@@ -26,30 +29,6 @@ export async function searchLibrary(): Promise<AudioAssetWithMetadata[]> {
     const granted = await requestPermission();
     if (!granted) return [];
     // grab all audio files
-
-    // const songsResults = await getAll({
-    //     limit: 20,
-    //     offset: 0,
-    //     coverQuality: 50,
-    //     minSongDuration: 1000,
-    //     sortOrder: SortSongOrder.DESC,
-    //     sortBy: SortSongFields.TITLE,
-    // });
-
-    // if (typeof songsResults === 'string') {
-    //     console.error('Error getting music files:', songsResults);
-    //     return [];
-    // }
-
-    // return songsResults.map((file: any): AudioAssetWithMetadata => ({
-    //     id: file.id || file.path,
-    //     uri: file.path,
-    //     artist: file.artist || 'Unknown Artist',
-    //     album: file.album || 'Unknown Album',
-    //     title: file.title || file.fileName,
-    //     genre: file.genre || 'Unknown Genre',
-    //     year: file.year || '',
-    // }));
 
     const media = await MediaLibrary.getAssetsAsync({
         mediaType: 'audio',
@@ -77,13 +56,33 @@ export async function searchLibrary(): Promise<AudioAssetWithMetadata[]> {
     // )
     // return grabExif;
 
-    return media.assets.map((asset): AudioAssetWithMetadata => ({
-        id: asset.id,
-        uri: asset.uri,
-        artist: 'Unknown Artist',
-        album: 'Unknown Album',
-        title: asset.filename.replace(/\.[^/.]+$/, ''),
-        genre: 'Unknown Genre',
-        year: '',
-    }));
+    //lets grab their metadata too
+    const grabExif = Promise.allSettled(
+        media.assets.map(async (asset): Promise<AudioAssetWithMetadata> => {
+            const base64 = await RNFS.readFile(asset.uri.replace('file://', ''), 'base64');
+            const buffer = Uint8Array.from(Buffer.from(base64, 'base64'));
+            const metadata = await mm.parseBuffer(buffer, 'audio/mpeg', { duration: false });
+            // print common tags only
+            // console.log('--- Common tags ---');
+            // console.log(metadata.common);
+            return {
+                id: asset.id,
+                uri: asset.uri,
+                artist: metadata.common.artist || 'Unknown Artist',
+                album: metadata.common.album || 'Unknown Album',
+                title: metadata.common.title || asset.filename,
+                picture: metadata.common.picture?.[0],
+            };
+        })
+    )
+    const result: Promise<AudioAssetWithMetadata[]> = grabExif
+        .then((results: PromiseSettledResult<AudioAssetWithMetadata>[]) =>
+            results
+                .filter(
+                    (r): r is PromiseFulfilledResult<AudioAssetWithMetadata> => r.status === 'fulfilled'
+                )
+                .map((r) => r.value)
+        );
+    return result;
+
 }
