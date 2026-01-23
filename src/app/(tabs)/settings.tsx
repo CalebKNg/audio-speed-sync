57b6e1ffb200798b3d4096888e30a8d92eeeb3bc
CalebKNg <caleb17ng@gmail.com>
2026-01-22 17:54:47 -0800

    add rest cache feature

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useAudioLibrary } from '../../functions/scanLibrary';
 import { common } from "../../styles/common";
 
+
 export default function Settings() {
+    const { clearCache } = useAudioLibrary();
     return (
         <View style={common.pageView}>
             <Text style={styles.text}>setting</Text>
-
+            <Button title='reset Cache' onPress={clearCache} />
+            <View />
         </View>
     )
 }
